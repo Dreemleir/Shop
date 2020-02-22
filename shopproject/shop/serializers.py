9893823ead090db0862283f05c3726435f6fae03
Dreemleir <dreemleir@gmail.com>
2020-02-22 20:51:18 +0200

    Update serializers.py

--- shopproject/shop/serializers.py
+++ shopproject/shop/serializers.py
@@ -3,6 +3,13 @@ from rest_framework import serializers
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    image_URL = serializers.SerializerMethodField()
+    
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'size', 'currency', 'imageURL']
+        fields = ['id', 'name', 'price', 'size', 'currency', 'image_URL']
+
+    def get_image_URL(self, products):
+        request = self.context.get('request')
+        image_URL = products.imageURL.url
+        return request.build_absolute_uri(image_URL)
